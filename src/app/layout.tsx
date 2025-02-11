e662bf3c4260519ce1eb609f2405c6928b74cca3
HyeYeong <h.lee@courier.jpn.com>
2025-02-11 16:25:34 +0900

    chor: 초기설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR&family=Noto+Sans+JP:wght@100..900&family=Oswald&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
